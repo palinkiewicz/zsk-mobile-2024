7d0acdbb0ead0e560588b965a053bbf1375e88d4
palinkiewicz <pogromca.ap@gmail.com>
2024-09-18 08:39:22 +0200

    Add shitty task 4.2 solution

--- 03_matura_java/src/main/java/org/example/Main.java
+++ 03_matura_java/src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -18,7 +20,11 @@ public class Main {
 
             wyniki4.write("Zadanie 4.1:\n");
             ArrayList<Integer> z41 = zadanie41(lines);
-            wyniki4.write(z41.size() + " " + z41.get(0));
+            wyniki4.write(z41.size() + " " + z41.get(0) + "\n");
+
+            wyniki4.write("Zadanie 4.2:\n");
+            List<Integer> z42 = zadanie42(lines);
+            wyniki4.write(z42.get(0) + " " + z42.get(1) + " " + z42.get(2) + " " + z42.get(3) + "\n");
 
             wyniki4.close();
         } catch (FileNotFoundException e) {
@@ -41,5 +47,41 @@ public class Main {
         }
 
         return results;
+    }
+
+    private static List<Integer> zadanie42(ArrayList<Integer> lines) {
+        int maxNum = 0;
+        int maxFactorials = 0;
+        int maxDistinctNum = 0;
+        int maxDistinctFactorials = 0;
+
+        List<Integer> primeNumbers = List.of(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103);
+
+        for (int num : lines) {
+            int num_copy = num;
+            int factCnt = 0;
+            int distFactCnt = 0;
+
+            for (int i = 0; num > 1 && i < primeNumbers.size(); i++) {
+
+                if (num % primeNumbers.get(i) == 0) distFactCnt++;
+                while (num % primeNumbers.get(i) == 0) {
+                    factCnt++;
+                    num /= primeNumbers.get(i);
+                }
+            }
+
+            if (distFactCnt > maxDistinctFactorials) {
+                maxDistinctNum = num_copy;
+                maxDistinctFactorials = distFactCnt;
+            }
+
+            if (factCnt > maxFactorials) {
+                maxNum = num_copy;
+                maxFactorials = factCnt;
+            }
+        }
+
+        return List.of(maxNum, maxFactorials, maxDistinctNum, maxDistinctFactorials);
     }
 }
