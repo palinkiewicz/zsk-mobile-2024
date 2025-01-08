fbafc75588f29f67edf72f292d1a346571aef710
palinkiewicz <pogromca.ap@gmail.com>
2025-01-08 08:41:29 +0100

    Complete half of the exam

---
+++ 14_android_egzamin/app/src/main/java/edu/zsk/vet/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.zsk.vet;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ListView list = (ListView) findViewById(R.id.type_list);
+
+        String[] animals = {"Pies", "Kot", "Świnka morska"};
+
+        ArrayList<String> animalsL = new ArrayList<>(Arrays.asList(animals));
+
+        list.setAdapter(new ArrayAdapter<>(this, R.layout.row, animalsL));
+    }
+}
